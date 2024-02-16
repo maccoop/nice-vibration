a09ff385e2810e4f9f32aa04b73574eddc9472e5
SangVu <sangvd@jacat.games>
2024-02-16 11:13:34 +0700

    project

---
+++ Assets/_Project/Scripts/Other/VibrationController.cs
@@ -0,0 +1,17 @@
+using Lofelt.NiceVibrations;
+using UnityEngine;
+
+public class VibrationController
+{
+    /// <summary>
+    /// tai sao lai la 0.1005f
+    /// </summary>
+    private const float DELAY_HAPTIC_SECOND = 0.1005f;
+    private static float nextTimePlay = 0;
+    public static void PlayByPreset(HapticPatterns.PresetType type)
+    {
+        if (Time.time < nextTimePlay) return;
+        HapticPatterns.PlayPreset(type);
+        nextTimePlay = DELAY_HAPTIC_SECOND + Time.time;
+    }
+}
